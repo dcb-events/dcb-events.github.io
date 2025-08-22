5ba0f510912ad0f7daaaf66e38f0d948edb82b8d
Bastian Waidelich <b.waidelich@wwwision.de>
2025-08-22 17:57:17 +0200

    Update hooks/copyright.py
    Co-authored-by: Golo Roden <golo.roden@thenativeweb.io>

--- hooks/copyright.py
+++ hooks/copyright.py
@@ -1,3 +1,3 @@
 from datetime import datetime
 def on_config(config, **kwargs):
-    config.copyright = f"&copy; {datetime.now().year} – Except where otherwise noted , content on this site is licensed under a <a href=\"https://creativecommons.org/licenses/by-sa/4.0/\" target=\"_blank\">Creative Commons Attribution-ShareAlike 4.0 International license</a>"
+    config.copyright = f"&copy; {datetime.now().year} – Except where otherwise noted, content on this site is licensed under a <a href=\"https://creativecommons.org/licenses/by-sa/4.0/\" target=\"_blank\">Creative Commons Attribution-ShareAlike 4.0 International license</a>"
